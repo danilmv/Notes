8d599ce4df0cc24a3411ca77de18eafb66635f66
danilmv <43599357+danilmv@users.noreply.github.com>
2021-06-05 18:19:02 +0300

    L6: Notes: Folders: add new folder

--- notes/src/main/java/com/andriod/notes/MainActivity.java
+++ notes/src/main/java/com/andriod/notes/MainActivity.java
@@ -3,11 +3,13 @@ package com.andriod.notes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.Toast;
 
 import com.andriod.notes.entity.Note;
 import com.andriod.notes.fragment.Controller;
+import com.andriod.notes.fragment.FolderAddNewFragment;
 import com.andriod.notes.fragment.FolderFragment;
 
 import java.util.ArrayList;
@@ -15,6 +17,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements Controller {
 
+    private static final String TAG = "@MainActivity@";
     private List<String> folders = new ArrayList<>();
     private List<Note> notes = new ArrayList<>();
     private String currentFolder;
@@ -34,7 +37,7 @@ public class MainActivity extends AppCompatActivity implements Controller {
         return folders.size();
     }
 
-    public String getFolder(int index){
+    public String getFolder(int index) {
         return folders.get(index);
     }
 
@@ -44,13 +47,31 @@ public class MainActivity extends AppCompatActivity implements Controller {
 
     @Override
     public void folderPicked(int index) {
+        Log.d(TAG, "folderPicked() called with: index = [" + index + "]");
         currentFolder = getFolder(index);
         Toast.makeText(this, String.format("Folder picked: %s", currentFolder), Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void folderAddNew() {
+        Log.d(TAG, "folderAddNew()");
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, new FolderAddNewFragment())
+                .commit();
+    }
 
+    @Override
+    public void folderAddNewResult(String newFolderName) {
+        if (newFolderName != null) {
+            folders.add(newFolderName);
+            Log.d(TAG, String.format("folderAddNewResult() new folder added: %s", newFolderName));
+        }
+        Log.d(TAG, "folderAddNewResult() back to FolderFragment...");
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, new FolderFragment())
+                .commit();
     }
 
     @Override
--- notes/src/main/java/com/andriod/notes/fragment/Controller.java
+++ notes/src/main/java/com/andriod/notes/fragment/Controller.java
@@ -3,4 +3,5 @@ package com.andriod.notes.fragment;
 public interface Controller {
     void folderPicked(int index);
     void folderAddNew();
+    void folderAddNewResult(String newFolderName);
 }
