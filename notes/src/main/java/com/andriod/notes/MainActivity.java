37c09e88693f98d2727cf8e82e16508314fbcbf3
danilmv <43599357+danilmv@users.noreply.github.com>
2021-06-05 15:26:00 +0300

    L6: Notes: Folders: list, menu add

--- notes/src/main/java/com/andriod/notes/MainActivity.java
+++ notes/src/main/java/com/andriod/notes/MainActivity.java
@@ -3,12 +3,58 @@ package com.andriod.notes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.andriod.notes.entity.Note;
+import com.andriod.notes.fragment.Controller;
+import com.andriod.notes.fragment.FolderFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements Controller {
+
+    private List<String> folders = new ArrayList<>();
+    private List<Note> notes = new ArrayList<>();
+    private String currentFolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, new FolderFragment())
+                .commit();
+    }
+
+    public int getFolderSize() {
+        return folders.size();
+    }
+
+    public String getFolder(int index){
+        return folders.get(index);
+    }
+
+    public List<Note> getNotes() {
+        return notes;
+    }
+
+    @Override
+    public void folderPicked(int index) {
+        currentFolder = getFolder(index);
+        Toast.makeText(this, String.format("Folder picked: %s", currentFolder), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void folderAddNew() {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
---
+++ notes/src/main/java/com/andriod/notes/fragment/Controller.java
@@ -0,0 +1,6 @@
+package com.andriod.notes.fragment;
+
+public interface Controller {
+    void folderPicked(int index);
+    void folderAddNew();
+}
